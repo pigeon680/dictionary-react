be6c4e24456f9133f3180357c8264d800734fa80
WhitePigeon777 <whitepigeon0303@gmail.com>
2024-12-16 13:13:47 -0500

    dictionary

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchData from './components/SearchData';
+
+function App() {
+  return (
+    <div className="App1">
+        <SearchData/>
+    </div>
+  );
+}
+
+export default App;
